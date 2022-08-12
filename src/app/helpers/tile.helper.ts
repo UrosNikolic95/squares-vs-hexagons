6046d03efa09785cf1d7c77a0afe0f8e2a246e42
UrosNikolic95 <4.3.1995.urosnikolic@gmail.com>
2022-08-12 20:11:37 +0200

    locationUpdate

--- src/app/helpers/tile.helper.ts
+++ src/app/helpers/tile.helper.ts
@@ -6,22 +6,23 @@ const radiansPerDegree = radiansInCircle / degreesInCircle;
 export const triangleSide = 30;
 const triangleHeigth = (triangleSide * Math.sqrt(3)) / 2;
 
-const spacing = 2;
+export const spacing = 2;
 const diameter = triangleSide - spacing;
-export const points = Array.from({ length: 12 }, (value, index) => {
-  const diameterPrim = index % 2 == 0 ? diameter : triangleHeigth - spacing;
-  const degrees = index * 30 + 30;
-  const radians = degrees * radiansPerDegree;
-  const x = diameterPrim * Math.cos(radians) + triangleHeigth;
-  const y = diameterPrim * Math.sin(radians) + diameter;
-  return x.toFixed(2) + ',' + y.toFixed(2);
-}).join(' ');
+
+export function calculatePoints(shift = 0, modul = 2) {
+  return Array.from({ length: 12 }, (value, index) => {
+    const diameterPrim =
+      (index + shift) % modul == 0 ? diameter : triangleHeigth - spacing;
+    const degrees = index * 30 + 30;
+    const radians = degrees * radiansPerDegree;
+    const x = diameterPrim * Math.cos(radians) + triangleHeigth;
+    const y = diameterPrim * Math.sin(radians) + diameter;
+    return x.toFixed(2) + ',' + y.toFixed(2);
+  }).join(' ');
+}
 
 export const squareWigth = 2 * triangleHeigth;
 export const squareHeigth = 2 * triangleSide;
-
-export const heigth = 50;
-export const wigth = 50;
 
 export function generateFIeld(a: number, b = a) {
   return Array.from({ length: a }, (_, indexA) =>
--- src/app/single-tile-component/single-tile-component.component.ts
+++ src/app/single-tile-component/single-tile-component.component.ts
@@ -1,6 +1,13 @@
-import { Component, HostBinding, Input, OnInit } from '@angular/core';
 import {
-  points,
+  Component,
+  HostBinding,
+  HostListener,
+  Input,
+  OnInit,
+} from '@angular/core';
+import {
+  calculatePoints,
+  spacing,
   squareHeigth,
   squareWigth,
   triangleSide,
@@ -15,13 +22,26 @@ export class SingleTileComponentComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {
-    const x = this?.x || this.point.x;
-    const y = this?.y || this.point.y;
+    this.locationUpdate();
+  }
 
-    const shift = y % 2 == 0 ? 0 : this.width / 2;
+  locationUpdate() {
+    const x = this.point.x;
+    const y = this.point.y;
 
-    this.x = x * this.width + shift;
-    this.y = (y * triangleSide * 3) / 2;
+    if (this.shift % 2 == 0) {
+      const shift = y % 2 == 0 ? 0 : squareWigth / 2;
+      this.x = x * squareWigth + shift;
+      this.y = (y * triangleSide * 3) / 2;
+      this.height = squareHeigth;
+      this.width = squareWigth;
+    } else {
+      const shift = x % 2 == 0 ? 0 : squareWigth / 2;
+      this.x = (x * triangleSide * 3) / 2;
+      this.y = y * squareWigth + shift;
+      this.height = squareWigth;
+      this.width = squareHeigth;
+    }
   }
 
   @Input()
@@ -34,9 +54,26 @@ export class SingleTileComponentComponent implements OnInit {
 
   @HostBinding('style.left.px')
   x?: number = 0;
-
   @HostBinding('style.top.px')
   y?: number = 0;
 
-  points = points;
+  shift = 0;
+  modul = 2;
+
+  points = calculatePoints(this.shift);
+
+  @HostListener('document:keyup', ['$event.target', '$event'])
+  keydown(element: Element, event: KeyboardEvent) {
+    console.log(event.key, this.shift);
+    if (event.key == '1') {
+      this.shift++;
+      this.points = calculatePoints(this.shift, this.modul);
+      this.locationUpdate();
+    }
+    if (event.key == '2') {
+      this.modul = this.modul == 2 ? 3 : 2;
+      this.points = calculatePoints(this.shift, this.modul);
+      this.locationUpdate();
+    }
+  }
 }
